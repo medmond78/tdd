e906335cfba8c137660baba5e097bbcf29f4bcaf
Medmond78 <pauslema@gmail.com>
2024-06-06 15:25:41 -0400

    Updated with usage example

--- getTime.py
+++ getTime.py
@@ -1,16 +1,18 @@
-def getMillis():
-    # type: () -> object
-    import pytz
+def getMillis() -> int:
     from datetime import datetime
     import time
 
-    #Print today's date and time in UTC
-    tdd = datetime.utcnow().replace(tzinfo=pytz.utc)
-    print(tdd)
+    # Print today's date and time in UTC
+    tdd = datetime.utcnow()
+    print("Current UTC time:", tdd)
 
-    #Print today's date in milliseconds since 1/1/1970
+    # Print today's date in milliseconds since 1/1/1970
     millis = int(round(time.time() * 1000))
-    #print(millis)
+    print("Milliseconds since Unix epoch:", millis)
+    
     return millis
 
-
+# Test the getMillis function
+if __name__ == "__main__":
+    millis = getMillis()
+    print("Returned milliseconds:", millis)
